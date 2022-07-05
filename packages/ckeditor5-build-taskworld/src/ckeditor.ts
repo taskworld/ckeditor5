d412cbb08ed0cfe32cbcc4244b6570e5d4083c67
Anantachai Saothong (Manta) <anantachai.s@taskworld.com>
2022-07-06 00:08:50 +0700

    Add horizontal line feature

--- packages/ckeditor5-build-taskworld/src/ckeditor.ts
+++ packages/ckeditor5-build-taskworld/src/ckeditor.ts
@@ -11,6 +11,7 @@ import { BlockQuote } from '../../ckeditor5-block-quote/src/index.js';
 import { CodeBlock } from '../../ckeditor5-code-block/src/index.js';
 import { Essentials } from '../../ckeditor5-essentials/src/index.js';
 import { Heading } from '../../ckeditor5-heading/src/index.js';
+import { HorizontalLine } from '../../ckeditor5-horizontal-line/src/index.js';
 import { Indent } from '../../ckeditor5-indent/src/index.js';
 import { AutoLink, Link } from '../../ckeditor5-link/src/index.js';
 import { List } from '../../ckeditor5-list/src/index.js';
@@ -32,6 +33,7 @@ ClassicEditor.builtinPlugins = [
 	CodeBlock,
 	Essentials,
 	Heading,
+	HorizontalLine,
 	Italic,
 	Indent,
 	Link,
