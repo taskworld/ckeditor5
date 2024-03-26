26708773d819861ad45f7be1af4162dd82091e9d
Anantachai Saothong (Manta) <anantachai.s@taskworld.com>
2024-03-26 19:21:23 +0700

    Add @mention support

--- packages/ckeditor5-markdown-gfm/src/gfmdataprocessor.ts
+++ packages/ckeditor5-markdown-gfm/src/gfmdataprocessor.ts
@@ -14,6 +14,7 @@ import {
 	type ViewDocumentFragment,
 	type MatcherPattern
 } from 'ckeditor5/src/engine.js';
+import escape from 'lodash-es/escape.js';
 
 import { MarkdownToHtml } from './markdown2html/markdown2html.js';
 import { HtmlToMarkdown } from './html2markdown/html2markdown.js';
@@ -29,6 +30,8 @@ export default class GFMDataProcessor implements DataProcessor {
 	 */
 	private _htmlDP: HtmlDataProcessor;
 
+	private mentionIdToText?: ( mentionId: string ) => string;
+
 	/**
 	 * Helper for converting Markdown to HTML.
 	 */
@@ -42,10 +45,12 @@ export default class GFMDataProcessor implements DataProcessor {
 	/**
 	 * Creates a new instance of the Markdown data processor class.
 	 */
-	constructor( document: ViewDocument ) {
+	constructor( document: ViewDocument, mentionIdToText?: ( mentionId: string ) => string ) {
 		this._htmlDP = new HtmlDataProcessor( document );
 		this._markdown2html = new MarkdownToHtml();
 		this._html2markdown = new HtmlToMarkdown();
+
+		this.mentionIdToText = mentionIdToText;
 	}
 
 	/**
@@ -68,7 +73,15 @@ export default class GFMDataProcessor implements DataProcessor {
 	 */
 	public toView( data: string ): ViewDocumentFragment {
 		const html = this._markdown2html.parse( data );
-		return this._htmlDP.toView( html );
+
+		// Converts `@:5f7e99c932cb8c00061b87d9:` to `<span class="mention" data-mention="@5f7e99c932cb8c00061b87d9">@Patrick Star</span>`
+		const htmlWithMentions = html.replace(
+			/@:([0-9a-fA-F]{24}):/g,
+			( match, id ) => `<span class="mention" data-mention="@${ id }">@${ escape( typeof this.mentionIdToText === 'function' ? this.mentionIdToText( id ) : id )
+			}</span>`
+		);
+
+		return this._htmlDP.toView( htmlWithMentions );
 	}
 
 	/**
@@ -79,7 +92,36 @@ export default class GFMDataProcessor implements DataProcessor {
 	 */
 	public toData( viewFragment: ViewDocumentFragment ): string {
 		const html = this._htmlDP.toData( viewFragment );
-		return this._html2markdown.parse( html );
+
+		// Converts `<span class="mention" data-mention="@5f7e99c932cb8c00061b87d9">@Patrick Star</span>` to `@:5f7e99c932cb8c00061b87d9:`
+		// See https://github.com/taskworld/ckeditor5/blob/84700cd93725130eb28abc1657b058a62b1d662d/packages/ckeditor5-mention/src/mentionediting.js#L140-L141
+		const parser = new window.DOMParser();
+		const holder = parser.parseFromString( html, 'text/html' );
+		const walker = holder.createTreeWalker(
+			holder.body,
+			window.NodeFilter.SHOW_ELEMENT,
+			{
+				acceptNode: node =>
+					node instanceof HTMLElement &&
+						( node.classList.contains( 'mention' ) && node.getAttribute( 'data-mention' ) ) ?
+						window.NodeFilter.FILTER_ACCEPT : window.NodeFilter.FILTER_SKIP
+			}
+		);
+		let node;
+		// eslint-disable-next-line no-constant-condition
+		while ( true ) {
+			node = walker.nextNode();
+			if ( node === null ) {
+				break;
+			}
+
+			// See https://github.com/taskworld/tw-backend/blob/2c30299fe57fd41a43ca4990377b8503a23266c8/app/legacy/api/message-service/MentionExtractor.ts#L9
+			if ( node instanceof HTMLElement && node.hasAttribute( 'data-mention' ) ) {
+				node.innerText = '@:' + node.getAttribute( 'data-mention' )!.replace( /^@/, '' ) + ':';
+			}
+		}
+
+		return this._html2markdown.parse( holder.body.innerHTML );
 	}
 
 	/**
@@ -100,5 +142,5 @@ export default class GFMDataProcessor implements DataProcessor {
 	 * This method does not have any effect on the data processor result. It exists for compatibility with the
 	 * {@link module:engine/dataprocessor/dataprocessor~DataProcessor `DataProcessor` interface}.
 	 */
-	public useFillerType(): void {}
+	public useFillerType(): void { }
 }
